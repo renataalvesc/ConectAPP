1879177e392ce4b0384cc0612a993fbdaaa95bdb
Renata Moura <renatamouradev@gmail.com>
2024-11-17 18:32:37 -0300

    profile changes 2

--- app/{auth)/profile.tsx
+++ app/{auth)/profile.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput} from "react-native";
+import { StyleSheet, Text, View, TextInput, Button} from "react-native";
 import{useUser} from '@clerk/clerk-expo'
 import { useState } from "react";
 
@@ -8,11 +8,42 @@ export default function Profile(){
     const[firstName, setFistName] = useState("user?.firstName")
     const[lastName, setLastName] = useState("user?.lastName")
 
+    async function handUpadeProfile(){
+        try{
+            const result = await user?.update({
+                firstName: firstName,
+                lastName: lastName
+
+            })
+            console.log("USUARIO ATUALIZADO", result)
+
+        }catch(err){
+            console.log("ERRO AO ATUALIZAR! ", err)
+        }
+    }
+
     return(
-        <View>
+        <View style = {styles.container}>
             {user?.fullName && (
                 <Text>{user?.fullName}</Text>
             )}
+            <TextInput
+                value={firstName}
+                onChangeText={setFistName}
+                placeholder="Digite seu primeiro nome..."
+                style = {styles.input}
+            />
+             <TextInput
+                value={lastName}
+                onChangeText={setLastName}
+                placeholder="Digite seu sobrenome..."
+                style = {styles.input}
+            />
+            <Button
+                title="Atualizar perfil"
+                onPress={handUpadeProfile}
+                color="#3366FF"
+            />
         </View>
     )
 }
@@ -21,8 +52,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         padding: 40,
-    }
-    input:{
+    },
+    input: {
         marginVertical: 4,
         height: 50,
         borderWidth: 1,
