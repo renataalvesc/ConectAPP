3dc67d41bb8412f75b36984abe233e87ff2e480a
Renata Moura <renatamouradev@gmail.com>
2024-11-17 17:45:18 -0300

    home

--- app/{auth)/home.tsx
+++ app/{auth)/home.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
+import { useUser} from "@clerk/clerk-expo"
+import { Link } from "expo-router";
 
 export default function Home(){
+    const{user} = useUser();
     return(
         <View>
-            <Text>
-                Pagina Home
-            </Text>
+            <Text>Pagina Home</Text>
+            <Text>Email:{user?.emailAddresses[0].emailAddress}</Text>
 
+            <Link href="/{auth)/profile">Meu perfil</Link>
+            
         </View>
 
     )
