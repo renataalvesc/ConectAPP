4f2a28a2080e43609c947d3a622f11f79e76d9da
Renata Moura <renatamouradev@gmail.com>
2024-11-17 17:44:52 -0300

    profile

--- app/{auth)/profile.tsx
+++ app/{auth)/profile.tsx
@@ -1,16 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput} from "react-native";
+import{useUser} from '@clerk/clerk-expo'
+import { useState } from "react";
 
 export default function Profile(){
+    const { user} = useUser()
+
+    const[firstName, setFistName] = useState("user?.firstName")
+    const[lastName, setLastName] = useState("user?.lastName")
+
     return(
         <View>
-            <Text>
-                Pagina Perfil
-            </Text>
-
+            {user?.fullName && (
+                <Text>{user?.fullName}</Text>
+            )}
         </View>
-
     )
 }
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 40,
+    }
+    input:{
+        marginVertical: 4,
+        height: 50,
+        borderWidth: 1,
+        borderColor: '#121212',
+        borderRadius: 8,
+        padding: 10,
+        backgroundColor: '#fff'
+    
+      },
     
 })
